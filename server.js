905ffc48e06a747493675977c4616d913d0f09dc
hastytoro <luigi.lupini@gmail.com>
2021-02-19 09:06:19 +0200

    express-caching build.

--- server.js
+++ server.js
@@ -8,10 +8,23 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => res.send('home route.'));
-app.get('/signin', (req, res) => res.send('signin route.'));
-app.get('/register', (req, res) => res.send('register route.'));
-app.get('/about', (req, res) => res.send('about route.'));
+// caching option one:
+app.use(
+  '/static',
+  express.static(path.join(__dirname, 'public'), { maxAge: '2h' }),
+);
+
+// caching option two:
+app.get('/test', (req, res) => {
+  res.header('Cache-Control', 'public, max-age=86400');
+  res.header('Content-Type', 'text/html');
+  res.send('<h2>Test string</h2>');
+});
+
+// app.get('/', (req, res) => res.send('home route.'));
+// app.get('/signin', (req, res) => res.send('signin route.'));
+// app.get('/register', (req, res) => res.send('register route.'));
+// app.get('/about', (req, res) => res.send('about route.'));
 
 const PORT = process.env.PORT;
 app.listen(PORT || 3000, () => console.log(`app is running on port 3000.`));
