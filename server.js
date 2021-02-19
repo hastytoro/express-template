c8596bcbae00ce680c859218ec99a20167e54c13
hastytoro <luigi.lupini@gmail.com>
2021-02-19 10:44:07 +0200

    express-docker-nginx-load-balancer

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const os = require('os');
 const path = require('path');
 const knex = require('knex');
 
@@ -8,23 +9,17 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// caching option one:
-app.use(
-  '/static',
-  express.static(path.join(__dirname, 'public'), { maxAge: '2h' }),
-);
-
-// caching option two:
-app.get('/test', (req, res) => {
-  res.header('Cache-Control', 'public, max-age=86400');
-  res.header('Content-Type', 'text/html');
-  res.send('<h2>Test string</h2>');
+// home routes:
+app.get('/', (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+  console.log('I have been hit!');
+  res.end(
+    JSON.stringify({
+      message: req.socket.remoteAddress,
+      net: os.networkInterfaces(),
+    }),
+  );
 });
-
-// app.get('/', (req, res) => res.send('home route.'));
-// app.get('/signin', (req, res) => res.send('signin route.'));
-// app.get('/register', (req, res) => res.send('register route.'));
-// app.get('/about', (req, res) => res.send('about route.'));
 
 const PORT = process.env.PORT;
 app.listen(PORT || 3000, () => console.log(`app is running on port 3000.`));
